cf473018872796fd6cc5d3d9e73e7d64bc5afc0b
Tristan Van Berkom <tvb@src.gnome.org>
2005-12-06 16:43:34 +0000

    Modified Files: ChangeLog po/az.po po/en_CA.po po/es.po po/ja.po po/ms.po
    Modified Files:
     	ChangeLog po/az.po po/en_CA.po po/es.po po/ja.po po/ms.po
     	po/nn.po po/no.po po/pl.po po/pt.po po/ru.po po/sv.po po/ta.po
     	po/tr.po po/uk.po po/zh_CN.po src/glade-app.c src/glade-app.h
     	src/glade-catalog.c src/glade-catalog.h src/glade-command.c
     	src/glade-cursor.c src/glade-debug.c
     	src/glade-editor-property.c src/glade-editor.c src/glade-gtk.c
     	src/glade-id-allocator.c src/glade-palette.c
     	src/glade-parameter.c src/glade-placeholder.c
     	src/glade-popup.c src/glade-project-view.c
     	src/glade-project-window.c src/glade-project.c
     	src/glade-property-class.c src/glade-property.c
     	src/glade-signal-editor.c src/glade-signal.c src/glade-utils.c
     	src/glade-widget-class.c src/glade-widget.c src/main.c
    
      Updated land address for the Free Software Foundation in Boston.

--- src/glade-project-window.c
+++ src/glade-project-window.c
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * Authors:
  *   Chema Celorio <chema@celorio.com>
@@ -1000,7 +1000,8 @@ static void gpw_about_cb (GtkAction *action, GladeProjectWindow *gpw)
 		  "GNU General Public License for more details.\n\n"
 		  "You should have received a copy of the GNU General Public License\n"
 		  "along with this program; if not, write to the Free Software\n"
-		  "Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.");
+		  "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, "
+		  "MA 02110-1301, USA.");
 
 	gchar *copyright =
 		"(C) 2001-2005 Ximian, Inc.\n"
