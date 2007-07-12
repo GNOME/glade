4d2597b21076c74a324f7329cbd7da00f2ff0ead
Vincent Geddes <vgeddes@src.gnome.org>
2007-07-12 19:08:40 +0000

    updated tests
    	* updated tests
    
    svn path=/branches/BINDINGS/; revision=1463

--- tests/pythonplugin.py
+++ tests/pythonplugin.py
@@ -1,31 +1,33 @@
-import gtk
 import gobject
-import glade
+import gtk
+import gladeui
 
-class GladeMyBoxAdaptor(glade.get_adaptor_for_type ('GtkHBox')):
-        __gtype_name__ = 'GladeMyBoxAdaptor'
+class MyBoxAdaptor(gladeui.get_adaptor_for_type('GtkHBox')):
+        __gtype_name__ = 'MyBoxAdaptor'
         
         def __init__(self):
-                glade.GladeGtkHBoxAdaptor.__init__(self)
+                gladeui.GladeGtkHBoxAdaptor.__init__(self)
+                print "__init__\n"
                 
         def do_post_create(self, obj, reason):
-                print "Hello Monty world!\n";
+                print "do_post_create\n"
                 
         def do_child_set_property(self, container, child, property_name, value):
-                glade.GladeGtkHBoxAdaptor.do_child_set_property(self,container, child, property_name, value);
-                print "Hello do_child_set_property\n";
+                gladeui.GladeGtkHBoxAdaptor.do_child_set_property(self, container, child, property_name, value)
+                print "do_child_set_property\n"
                 
         def do_child_get_property(self, container, child, property_name):
-                a = glade.GladeGtkHBoxAdaptor.do_child_get_property(self,container, child, property_name);
-                print "Hello do_child_get_property\n";
-                return a;
+                a = gladeui.GladeGtkHBoxAdaptor.do_child_get_property(self, container, child, property_name)
+                print "do_child_get_property\n"
+                return a
                 
         def do_get_children(self, container):
-                a = glade.GladeGtkHBoxAdaptor.do_get_children(self, container);
-                print "Hello do_get_children\n";
-                return a;
+                a = gladeui.GladeGtkHBoxAdaptor.do_get_children(self, container)
+                print "do_get_children\n"
+                return a
 
 class MyBox(gtk.HBox):
         __gtype_name__ = 'MyBox'
+        
         def __init__(self):
                 gtk.HBox.__init__(self)
