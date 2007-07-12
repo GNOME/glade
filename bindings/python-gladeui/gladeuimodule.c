334d9c0a654d5f93a002ff92162b1cdf1707d6c9
Vincent Geddes <vgeddes@src.gnome.org>
2007-07-12 21:21:29 +0000

    update gladeui bindings
    	* update gladeui bindings
    
    svn path=/branches/BINDINGS/; revision=1466

--- bindings/python-gladeui/gladeuimodule.c
+++ bindings/python-gladeui/gladeuimodule.c
@@ -3,22 +3,25 @@
 
 #include <pygobject.h>
  
-void gladeui_register_classes (PyObject *d); 
+void gladeui_register_classes (PyObject *d);
+void gladeui_add_constants(PyObject *module, const gchar *strip_prefix);
 extern PyMethodDef gladeui_functions[];
  
 DL_EXPORT(void)
 init_gladeui(void)
 {
-    PyObject *m, *d;
+	PyObject *m, *d;
  
-    init_pygobject ();
+	init_pygobject ();
  
-    m = Py_InitModule ("_gladeui", gladeui_functions);
-    d = PyModule_GetDict (m);
+	m = Py_InitModule ("_gladeui", gladeui_functions);
+
+	gladeui_dict = d = PyModule_GetDict (m); 
  
-    gladeui_register_classes (d);
+	gladeui_register_classes (d);
+	gladeui_add_constants (m, "GLADE_");
  
-    if (PyErr_Occurred ()) {
-        Py_FatalError ("can't initialise module gladeui");
-    }
+	if (PyErr_Occurred ()) {
+		Py_FatalError ("can't initialise module gladeui");
+	}
 }
