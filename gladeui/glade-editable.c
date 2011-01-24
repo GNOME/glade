e8d3cac9dc20405ad601643e506399bf36d0fde1
Tristan Van Berkom <tristan.van.berkom@gmail.com>
2011-01-24 17:37:35 +0900

    * 41 files: Fixed typo in LGPL License file headers.

--- gladeui/glade-editable.c
+++ gladeui/glade-editable.c
@@ -7,7 +7,7 @@
  * Authors:
  *   Tristan Van Berkom <tvb@gnome.org>
  *
- * This library is free software; you can redistribute it and/or it
+ * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
--- plugins/gtk+/glade-convert.c
+++ plugins/gtk+/glade-convert.c
@@ -4,7 +4,7 @@
  *
  * Copyright (C) 2008 Tristan Van Berkom
  *
- * This library is free software; you can redistribute it and/or it
+ * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
