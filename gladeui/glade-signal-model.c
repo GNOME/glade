37f2be71b421cb94d6bc06fabd37809147ce8017
Tristan Van Berkom <tristan.van.berkom@gmail.com>
2011-01-24 17:36:13 +0900

    * 41 files: Fixed typo in LGPL License file headers.

--- gladeui/glade-signal-model.c
+++ gladeui/glade-signal-model.c
@@ -2,7 +2,7 @@
  * glade3
  * Copyright (C) Johannes Schmid 2010 <jhs@gnome.org>
  * 
- * This library is free software; you can redistribute it and/or it
+ * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
