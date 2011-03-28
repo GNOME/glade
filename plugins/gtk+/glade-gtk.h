a2bcc5f8f767ad49f7cd9fec87eca2fe2961e4c7
Tristan Van Berkom <tristan.van.berkom@gmail.com>
2011-03-28 11:25:56 -0300

    plugins/gtk+/glade-gtk.h: moved constant macros to common header plugins/gtk+/glade-gtk-activatable.h: added GtkActivatable declaration

---
+++ plugins/gtk+/glade-gtk.h
@@ -0,0 +1,44 @@
+/*
+ * glade-gtk.h
+ *
+ * Copyright (C) 2004 - 2011 Tristan Van Berkom.
+ *
+ * Author:
+ *   Tristan Van Berkom <tvb@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef __GLADE_GTK_H__
+#define __GLADE_GTK_H__
+
+G_BEGIN_DECLS
+
+/* --------------------------------- Constants ------------------------------ */
+
+#define FIXED_DEFAULT_CHILD_WIDTH  100
+#define FIXED_DEFAULT_CHILD_HEIGHT 60
+
+#define MNEMONIC_INSENSITIVE_MSG   _("This property does not apply unless Use Underline is set.")
+#define NOT_SELECTED_MSG           _("Property not selected")
+#define RESPID_INSENSITIVE_MSG     _("This property is only for use in dialog action buttons")
+#define ACTION_APPEARANCE_MSG      _("This property is set to be controlled by an Action")
+
+#define ONLY_THIS_GOES_IN_THAT_MSG _("Only objects of type %s can be added to objects of type %s.")
+
+G_END_DECLS
+
+#endif __GLADE_GTK_H__
