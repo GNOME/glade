37807efabe475e7c21beb98f4c5b7de4d121835a
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2011-12-15 17:14:05 -0300

    Removed dialog hint to make gnome-shell put a close button in the window

--- gladeui/glade-base-editor.c
+++ gladeui/glade-base-editor.c
@@ -2231,7 +2231,6 @@ glade_base_editor_pack_new_window (GladeBaseEditor * editor,
 
   /* Window */
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_DIALOG);
 
   if (title)
     {
