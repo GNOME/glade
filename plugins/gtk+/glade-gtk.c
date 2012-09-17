00f152c541cc9af29c98095441e4d6085168fa65
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2012-09-17 20:08:21 -0300

    Updated GtkAssistant support Fixed bug #664276 "Can't add widgets to a GtkAssistant" Make assistant update current page when a child is selected in the project.

--- plugins/gtk+/glade-gtk.c
+++ plugins/gtk+/glade-gtk.c
@@ -8045,10 +8045,8 @@ glade_gtk_assistant_post_create (GladeWidgetAdaptor * adaptor,
       g_signal_connect (project, "parse-finished",
                         G_CALLBACK (glade_gtk_assistant_parse_finished),
                         object);
-      return;
     }
-
-  if (reason == GLADE_CREATE_USER)
+  else if (reason == GLADE_CREATE_USER)
     {
       glade_gtk_assistant_append_new_page (parent, project,
                                            _("Introduction page"),
@@ -8161,7 +8159,10 @@ glade_gtk_assistant_set_property (GladeWidgetAdaptor * adaptor,
 
       for (i = gtk_assistant_get_n_pages (GTK_ASSISTANT (object)),
            size = g_value_get_int (value); i < size; i++)
+	{
+	  g_message ("aaaa %d %d", i,size);
         gtk_assistant_append_page (assistant, glade_placeholder_new ());
+	}
 
       glade_gtk_assistant_update_page_type (assistant);
 
