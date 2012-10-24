1eb39af041389508f78801107705e110265ad632
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2012-10-24 15:04:32 -0700

    Added missing prototype

--- gladeui/glade-design-private.h
+++ gladeui/glade-design-private.h
@@ -30,6 +30,10 @@ G_BEGIN_DECLS
 void _glade_design_view_freeze (GladeDesignView *view);
 void _glade_design_view_thaw   (GladeDesignView *view);
 
+void _glade_design_layout_get_colors (GtkStyleContext *context, 
+                                      GdkRGBA *c1, GdkRGBA *c2,
+                                      GdkRGBA *c3, GdkRGBA *c4);
+
 void _glade_design_layout_draw_node (cairo_t *cr,
                                      gdouble x,
                                      gdouble y,
