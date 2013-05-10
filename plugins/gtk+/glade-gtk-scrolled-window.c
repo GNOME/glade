87986f29c894d54d93b18f08410e9b49a9d31cc4
Tristan Van Berkom <tristan.van.berkom@gmail.com>
2013-05-10 23:24:59 +0900

    Added GladeScrolledWindowEditor

---
+++ plugins/gtk+/glade-gtk-scrolled-window.c
@@ -0,0 +1,60 @@
+/*
+ * glade-gtk-scrolled-window.c - GladeWidgetAdaptor for GtkScrolledWindow
+ *
+ * Copyright (C) 2013 Tristan Van Berkom
+ *
+ * Authors:
+ *      Tristan Van Berkom <tristan.van.berkom@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include <config.h>
+#include <glib/gi18n-lib.h>
+#include <gladeui/glade.h>
+
+#include "glade-scrolled-window-editor.h"
+
+GladeEditable *
+glade_gtk_scrolled_window_create_editable (GladeWidgetAdaptor * adaptor,
+					   GladeEditorPageType type)
+{
+  if (type == GLADE_PAGE_GENERAL)
+    {
+      return glade_scrolled_window_editor_new ();
+    }
+
+  return GWA_GET_CLASS (GTK_TYPE_CONTAINER)->create_editable (adaptor, type);
+}
+
+void
+glade_gtk_scrolled_window_set_property (GladeWidgetAdaptor * adaptor,
+					GObject * object,
+					const gchar * id, const GValue * value)
+{
+  GladeWidget *widget = glade_widget_get_from_gobject (object);
+  GladeProperty *property = glade_widget_get_property (widget, id);
+
+  if (strcmp (id, "window-placement-set") == 0)
+    {
+      if (g_value_get_boolean (value))
+	glade_widget_property_set_sensitive (widget, "window-placement", TRUE, NULL);
+      else
+	glade_widget_property_set_sensitive (widget, "window-placement", FALSE,
+					     _("This property is disabled"));
+    }
+  else if (GPC_VERSION_CHECK (glade_property_get_class (property), gtk_major_version, gtk_minor_version + 1))
+    GWA_GET_CLASS (GTK_TYPE_CONTAINER)->set_property (adaptor, object, id, value);
+}
---
+++ plugins/gtk+/glade-scrolled-window-editor.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2013 Tristan Van Berkom.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Authors:
+ *   Tristan Van Berkom <tvb@gnome.org>
+ */
+
+#include <config.h>
+#include <gladeui/glade.h>
+#include <glib/gi18n-lib.h>
+
+#include "glade-scrolled-window-editor.h"
+
+struct _GladeScrolledWindowEditorPrivate
+{
+  GtkWidget *embed;
+};
+
+static void glade_scrolled_window_editor_grab_focus (GtkWidget * widget);
+
+G_DEFINE_TYPE (GladeScrolledWindowEditor, glade_scrolled_window_editor, GLADE_TYPE_EDITOR_SKELETON)
+
+static void
+glade_scrolled_window_editor_class_init (GladeScrolledWindowEditorClass * klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  widget_class->grab_focus = glade_scrolled_window_editor_grab_focus;
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/gladegtk/glade-scrolled-window-editor.ui");
+  gtk_widget_class_bind_child (widget_class, GladeScrolledWindowEditorPrivate, embed);
+
+  g_type_class_add_private (object_class, sizeof (GladeScrolledWindowEditorPrivate));
+}
+
+static void
+glade_scrolled_window_editor_init (GladeScrolledWindowEditor * self)
+{
+  self->priv = 
+    G_TYPE_INSTANCE_GET_PRIVATE (self,
+				 GLADE_TYPE_SCROLLED_WINDOW_EDITOR,
+				 GladeScrolledWindowEditorPrivate);
+
+  gtk_widget_init_template (GTK_WIDGET (self));
+}
+
+static void
+glade_scrolled_window_editor_grab_focus (GtkWidget * widget)
+{
+  GladeScrolledWindowEditor *swindow_editor = GLADE_SCROLLED_WINDOW_EDITOR (widget);
+
+  gtk_widget_grab_focus (swindow_editor->priv->embed);
+}
+
+GtkWidget *
+glade_scrolled_window_editor_new (void)
+{
+  return g_object_new (GLADE_TYPE_SCROLLED_WINDOW_EDITOR, NULL);
+}
