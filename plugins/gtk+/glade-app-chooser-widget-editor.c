d7155dc95a9494f15740262a9147fdefecbdd24d
Tristan Van Berkom <tristan.van.berkom@gmail.com>
2013-05-12 20:26:15 +0900

    Added GladeAppChooserWidgetEditor

---
+++ plugins/gtk+/glade-app-chooser-widget-editor.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2013 Tristan Van Berkom.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Authors:
+ *   Tristan Van Berkom <tvb@gnome.org>
+ */
+
+#include <config.h>
+#include <gladeui/glade.h>
+#include <glib/gi18n-lib.h>
+
+#include "glade-app-chooser-widget-editor.h"
+
+static void glade_app_chooser_widget_editor_grab_focus (GtkWidget * widget);
+
+struct _GladeAppChooserWidgetEditorPrivate
+{
+  GtkWidget *embed;
+};
+
+G_DEFINE_TYPE (GladeAppChooserWidgetEditor, glade_app_chooser_widget_editor, GLADE_TYPE_EDITOR_SKELETON)
+
+static void
+glade_app_chooser_widget_editor_class_init (GladeAppChooserWidgetEditorClass * klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  widget_class->grab_focus = glade_app_chooser_widget_editor_grab_focus;
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/gladegtk/glade-app-chooser-widget-editor.ui");
+  gtk_widget_class_bind_child (widget_class, GladeAppChooserWidgetEditorPrivate, embed);
+
+  g_type_class_add_private (object_class, sizeof (GladeAppChooserWidgetEditorPrivate));  
+}
+
+static void
+glade_app_chooser_widget_editor_init (GladeAppChooserWidgetEditor * self)
+{
+  self->priv = 
+    G_TYPE_INSTANCE_GET_PRIVATE (self,
+				 GLADE_TYPE_APP_CHOOSER_WIDGET_EDITOR,
+				 GladeAppChooserWidgetEditorPrivate);
+
+  gtk_widget_init_template (GTK_WIDGET (self));
+}
+
+static void
+glade_app_chooser_widget_editor_grab_focus (GtkWidget * widget)
+{
+  GladeAppChooserWidgetEditor *app_editor = GLADE_APP_CHOOSER_WIDGET_EDITOR (widget);
+
+  gtk_widget_grab_focus (app_editor->priv->embed);
+}
+
+GtkWidget *
+glade_app_chooser_widget_editor_new (void)
+{
+  return g_object_new (GLADE_TYPE_APP_CHOOSER_WIDGET_EDITOR, NULL);
+}
---
+++ plugins/gtk+/glade-app-chooser-widget-editor.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2013 Tristan Van Berkom.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Authors:
+ *   Tristan Van Berkom <tvb@gnome.org>
+ */
+#ifndef _GLADE_APP_CHOOSER_WIDGET_EDITOR_H_
+#define _GLADE_APP_CHOOSER_WIDGET_EDITOR_H_
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define GLADE_TYPE_APP_CHOOSER_WIDGET_EDITOR	          (glade_app_chooser_widget_editor_get_type ())
+#define GLADE_APP_CHOOSER_WIDGET_EDITOR(obj)	          (G_TYPE_CHECK_INSTANCE_CAST ((obj), GLADE_TYPE_APP_CHOOSER_WIDGET_EDITOR, GladeAppChooserWidgetEditor))
+#define GLADE_APP_CHOOSER_WIDGET_EDITOR_CLASS(klass)	  (G_TYPE_CHECK_CLASS_CAST ((klass), GLADE_TYPE_APP_CHOOSER_WIDGET_EDITOR, GladeAppChooserWidgetEditorClass))
+#define GLADE_IS_APP_CHOOSER_WIDGET_EDITOR(obj)	  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GLADE_TYPE_APP_CHOOSER_WIDGET_EDITOR))
+#define GLADE_IS_APP_CHOOSER_WIDGET_EDITOR_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), GLADE_TYPE_APP_CHOOSER_WIDGET_EDITOR))
+#define GLADE_APP_CHOOSER_WIDGET_EDITOR_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), GLADE_TYPE_APP_CHOOSER_WIDGET_EDITOR, GladeAppChooserWidgetEditorClass))
+
+typedef struct _GladeAppChooserWidgetEditor        GladeAppChooserWidgetEditor;
+typedef struct _GladeAppChooserWidgetEditorClass   GladeAppChooserWidgetEditorClass;
+typedef struct _GladeAppChooserWidgetEditorPrivate GladeAppChooserWidgetEditorPrivate;
+
+struct _GladeAppChooserWidgetEditor
+{
+  GladeEditorSkeleton  parent;
+
+  GladeAppChooserWidgetEditorPrivate *priv;
+};
+
+struct _GladeAppChooserWidgetEditorClass
+{
+  GladeEditorSkeletonClass parent;
+};
+
+GType            glade_app_chooser_widget_editor_get_type (void) G_GNUC_CONST;
+GtkWidget       *glade_app_chooser_widget_editor_new      (void);
+
+G_END_DECLS
+
+#endif  /* _GLADE_APP_CHOOSER_WIDGET_EDITOR_H_ */
