22154af400771676e4a1e60598d41c80f7186eba
Tristan Van Berkom <tristan.van.berkom@gmail.com>
2013-05-15 23:17:47 +0900

    Added GladeScrollbarEditor

---
+++ plugins/gtk+/glade-scrollbar-editor.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2013 Tristan Van Berkom.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Authors:
+ *   Tristan Van Berkom <tvb@gnome.org>
+ */
+
+#include <config.h>
+#include <gladeui/glade.h>
+#include <glib/gi18n-lib.h>
+#include <gdk/gdkkeysyms.h>
+
+#include "glade-scrollbar-editor.h"
+
+static void glade_scrollbar_editor_grab_focus (GtkWidget * widget);
+
+struct _GladeScrollbarEditorPrivate
+{
+  GtkWidget *embed;
+};
+
+G_DEFINE_TYPE (GladeScrollbarEditor, glade_scrollbar_editor, GLADE_TYPE_EDITOR_SKELETON)
+
+static void
+glade_scrollbar_editor_class_init (GladeScrollbarEditorClass * klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  widget_class->grab_focus = glade_scrollbar_editor_grab_focus;
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/gladegtk/glade-scrollbar-editor.ui");
+  gtk_widget_class_bind_child (widget_class, GladeScrollbarEditorPrivate, embed);
+
+  g_type_class_add_private (object_class, sizeof (GladeScrollbarEditorPrivate));  
+}
+
+static void
+glade_scrollbar_editor_init (GladeScrollbarEditor * self)
+{
+  self->priv = 
+    G_TYPE_INSTANCE_GET_PRIVATE (self,
+				 GLADE_TYPE_SCROLLBAR_EDITOR,
+				 GladeScrollbarEditorPrivate);
+
+  gtk_widget_init_template (GTK_WIDGET (self));
+}
+
+static void
+glade_scrollbar_editor_grab_focus (GtkWidget * widget)
+{
+  GladeScrollbarEditor *scrollbar_editor = GLADE_SCROLLBAR_EDITOR (widget);
+
+  gtk_widget_grab_focus (scrollbar_editor->priv->embed);
+}
+
+GtkWidget *
+glade_scrollbar_editor_new (void)
+{
+  return g_object_new (GLADE_TYPE_SCROLLBAR_EDITOR, NULL);
+}
---
+++ plugins/gtk+/glade-scrollbar-editor.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2013 Tristan Van Berkom.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Authors:
+ *   Tristan Van Berkom <tvb@gnome.org>
+ */
+#ifndef _GLADE_SCROLLBAR_EDITOR_H_
+#define _GLADE_SCROLLBAR_EDITOR_H_
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define GLADE_TYPE_SCROLLBAR_EDITOR	            (glade_scrollbar_editor_get_type ())
+#define GLADE_SCROLLBAR_EDITOR(obj)		    (G_TYPE_CHECK_INSTANCE_CAST ((obj), GLADE_TYPE_SCROLLBAR_EDITOR, GladeScrollbarEditor))
+#define GLADE_SCROLLBAR_EDITOR_CLASS(klass)	    (G_TYPE_CHECK_CLASS_CAST ((klass), GLADE_TYPE_SCROLLBAR_EDITOR, GladeScrollbarEditorClass))
+#define GLADE_IS_SCROLLBAR_EDITOR(obj)	    (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GLADE_TYPE_SCROLLBAR_EDITOR))
+#define GLADE_IS_SCROLLBAR_EDITOR_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), GLADE_TYPE_SCROLLBAR_EDITOR))
+#define GLADE_SCROLLBAR_EDITOR_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), GLADE_TYPE_SCROLLBAR_EDITOR, GladeScrollbarEditorClass))
+
+typedef struct _GladeScrollbarEditor        GladeScrollbarEditor;
+typedef struct _GladeScrollbarEditorClass   GladeScrollbarEditorClass;
+typedef struct _GladeScrollbarEditorPrivate GladeScrollbarEditorPrivate;
+
+struct _GladeScrollbarEditor
+{
+  GladeEditorSkeleton  parent;
+
+  GladeScrollbarEditorPrivate *priv;
+};
+
+struct _GladeScrollbarEditorClass
+{
+  GladeEditorSkeletonClass parent;
+};
+
+GType            glade_scrollbar_editor_get_type (void) G_GNUC_CONST;
+GtkWidget       *glade_scrollbar_editor_new      (void);
+
+G_END_DECLS
+
+#endif  /* _GLADE_SCROLLBAR_EDITOR_H_ */
