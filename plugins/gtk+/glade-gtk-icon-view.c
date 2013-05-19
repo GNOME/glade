f7fc7903e476cd9df5a3ef09f2868ae754f49a8c
Tristan Van Berkom <tristan.van.berkom@gmail.com>
2013-05-19 17:03:34 +0900

    Added GladeIconViewEditor

---
+++ plugins/gtk+/glade-gtk-icon-view.c
@@ -0,0 +1,40 @@
+/*
+ * glade-gtk-icon-view.c - GladeWidgetAdaptor for GtkIconView
+ *
+ * Copyright (C) 2013 Tristan Van Berkom
+ *
+ * Authors:
+ *      Tristan Van Berkom <tristan.van.berkom@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the Free Softwarel
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include <config.h>
+#include <glib/gi18n-lib.h>
+#include <gladeui/glade.h>
+
+#include "glade-icon-view-editor.h"
+
+GladeEditable *
+glade_gtk_icon_view_create_editable (GladeWidgetAdaptor * adaptor,
+				     GladeEditorPageType type)
+{
+  if (type == GLADE_PAGE_GENERAL)
+    {
+      return (GladeEditable *)glade_icon_view_editor_new ();
+    }
+
+  return GWA_GET_CLASS (GTK_TYPE_CONTAINER)->create_editable (adaptor, type);
+}
