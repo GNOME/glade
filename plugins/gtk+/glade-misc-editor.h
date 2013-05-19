2264f1de4865acad3ebd7c9e9d0ec1b3018497fe
Tristan Van Berkom <tristan.van.berkom@gmail.com>
2013-05-19 17:48:32 +0900

    Added GladeMiscEditor
    The GladeMiscEditor edits the common GtkMisc properties in various editors,
    this initial commit makes use of the editor in GladeImageEditor.

---
+++ plugins/gtk+/glade-misc-editor.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2013 Tristan Van Berkom.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Authors:
+ *   Tristan Van Berkom <tvb@gnome.org>
+ */
+#ifndef _GLADE_MISC_EDITOR_H_
+#define _GLADE_MISC_EDITOR_H_
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define GLADE_TYPE_MISC_EDITOR	          (glade_misc_editor_get_type ())
+#define GLADE_MISC_EDITOR(obj)	          (G_TYPE_CHECK_INSTANCE_CAST ((obj), GLADE_TYPE_MISC_EDITOR, GladeMiscEditor))
+#define GLADE_MISC_EDITOR_CLASS(klass)	  (G_TYPE_CHECK_CLASS_CAST ((klass), GLADE_TYPE_MISC_EDITOR, GladeMiscEditorClass))
+#define GLADE_IS_MISC_EDITOR(obj)	  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GLADE_TYPE_MISC_EDITOR))
+#define GLADE_IS_MISC_EDITOR_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), GLADE_TYPE_MISC_EDITOR))
+#define GLADE_MISC_EDITOR_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), GLADE_TYPE_MISC_EDITOR, GladeMiscEditorClass))
+
+typedef struct _GladeMiscEditor        GladeMiscEditor;
+typedef struct _GladeMiscEditorClass   GladeMiscEditorClass;
+typedef struct _GladeMiscEditorPrivate GladeMiscEditorPrivate;
+
+struct _GladeMiscEditor
+{
+  GladeEditorSkeleton  parent;
+
+  GladeMiscEditorPrivate *priv;
+};
+
+struct _GladeMiscEditorClass
+{
+  GladeEditorSkeletonClass parent;
+};
+
+GType            glade_misc_editor_get_type (void) G_GNUC_CONST;
+GtkWidget       *glade_misc_editor_new      (void);
+
+G_END_DECLS
+
+#endif  /* _GLADE_MISC_EDITOR_H_ */
