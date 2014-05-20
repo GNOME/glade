76a8184f6f300d9e6d1a6b04eddf8fcd11a4638c
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2014-05-20 19:38:54 -0300

    glade-xml-utils.c: Add missing GPL license block.
    Close Bug 730285 "License issues"
    
    TODO: identify which function are subject to the GPL license and rewrite them.

--- gladeui/glade-xml-utils.c
+++ gladeui/glade-xml-utils.c
@@ -1,7 +1,22 @@
-/* This functions are based on gnome-print/libgpa/gpa-xml.c which were in turn 
- * based on gnumeric/xml-io.c
- */
-/* Authors:
+/*
+ * glade-xml-utils.c - This functions are based on gnome-print/libgpa/gpa-xml.c
+ * which were in turn based on gnumeric/xml-io.c
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+   *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Authors:
  *   Daniel Veillard <Daniel.Veillard@w3.org>
  *   Miguel de Icaza <miguel@gnu.org>
  *   Chema Celorio <chema@gnome.org>
