1889453af7ea4a361f6f5693352bfde76f73b3cc
Tristan Van Berkom <tristan@upstairslabs.com>
2014-07-21 12:36:03 -0300

    Bug 732575 - Changed the type hint on the "Edit Separately" window to 'utility'
    This window has traditionally had a 'dialog' type so that window
    managers don't treat it as a primary application window (don't
    add minimize/maximize, don't list it in the alt-tab list, etc).
    
    In light of bug 732575, where it seems some window managers have
    stolen the 'close' button away from windows with the 'dialog' hint,
    let's change it for a 'utility' hint and hope this is the more
    appropriate hint.

--- gladeui/glade-editor.c
+++ gladeui/glade-editor.c
@@ -1275,7 +1275,7 @@ glade_editor_dialog_for_widget (GladeWidget *widget)
 
   /* Window */
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_DIALOG);
+  gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_UTILITY);
 
   prj_name = glade_project_get_name (glade_widget_get_project (widget));
   /* Translators: first %s is the project name, second is a widget name */
