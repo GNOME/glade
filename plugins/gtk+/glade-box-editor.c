adc8f5b748fd4c66023ec2b2977e27061766f3d8
Matthias Clasen <mclasen@redhat.com>
2014-10-25 20:30:57 -0400

    Move use-center-child to the box editor
    This makes adding and removing center widgets undoable.

--- plugins/gtk+/glade-box-editor.c
+++ plugins/gtk+/glade-box-editor.c
@@ -26,14 +26,23 @@
 
 #include "glade-box-editor.h"
 
+static void glade_box_editor_editable_init (GladeEditableIface * iface);
 static void glade_box_editor_grab_focus (GtkWidget * widget);
+static void use_center_child_toggled (GtkWidget *widget, GladeBoxEditor * box_editor);
+
 
 struct _GladeBoxEditorPrivate
 {
   GtkWidget *embed;
+  GtkWidget *use_center_child;
 };
 
-G_DEFINE_TYPE_WITH_PRIVATE (GladeBoxEditor, glade_box_editor, GLADE_TYPE_EDITOR_SKELETON)
+static GladeEditableIface *parent_editable_iface;
+
+G_DEFINE_TYPE_WITH_CODE (GladeBoxEditor, glade_box_editor, GLADE_TYPE_EDITOR_SKELETON,
+                         G_ADD_PRIVATE (GladeBoxEditor)
+                         G_IMPLEMENT_INTERFACE (GLADE_TYPE_EDITABLE,
+                                                glade_box_editor_editable_init));
 
 static void
 glade_box_editor_class_init (GladeBoxEditorClass * klass)
@@ -44,6 +53,9 @@ glade_box_editor_class_init (GladeBoxEditorClass * klass)
 
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/gladegtk/glade-box-editor.ui");
   gtk_widget_class_bind_template_child_private (widget_class, GladeBoxEditor, embed);
+  gtk_widget_class_bind_template_child_private (widget_class, GladeBoxEditor, use_center_child);
+
+  gtk_widget_class_bind_template_callback (widget_class, use_center_child_toggled);
 }
 
 static void
@@ -60,6 +72,90 @@ glade_box_editor_grab_focus (GtkWidget * widget)
   GladeBoxEditor *box_editor = GLADE_BOX_EDITOR (widget);
 
   gtk_widget_grab_focus (box_editor->priv->embed);
+}
+
+static void
+glade_box_editor_load (GladeEditable *editable,
+                       GladeWidget   *gwidget)
+{
+  GladeBoxEditor *box_editor = GLADE_BOX_EDITOR (editable);
+  GladeBoxEditorPrivate *priv = box_editor->priv;
+
+  /* Chain up to default implementation */
+  parent_editable_iface->load (editable, gwidget);
+
+  if (gwidget)
+    {
+      gboolean use_center_child;
+
+      glade_widget_property_get (gwidget, "use-center-child", &use_center_child);
+
+      gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (priv->use_center_child), use_center_child);
+    }
+}
+
+static void
+glade_box_editor_editable_init (GladeEditableIface * iface)
+{
+  parent_editable_iface = g_type_interface_peek_parent (iface);
+
+  iface->load = glade_box_editor_load;
+}
+
+static void
+use_center_child_toggled (GtkWidget      * widget,
+                          GladeBoxEditor * box_editor)
+{
+  GladeBoxEditorPrivate *priv = box_editor->priv;
+  GladeWidget   *gwidget = glade_editable_loaded_widget (GLADE_EDITABLE (box_editor));
+  GladeWidget   *gcenter = NULL;
+  GtkWidget     *box;
+  GtkWidget     *center;
+  GladeProperty *property;
+  gboolean       use_center_child;
+
+  if (glade_editable_loading (GLADE_EDITABLE (box_editor)) || !gwidget)
+    return;
+
+  /* Get new desired property state */
+  use_center_child = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (priv->use_center_child));
+
+  /* Get any existing center child */
+  box = (GtkWidget *)glade_widget_get_object (gwidget);
+  center = gtk_box_get_center_widget (GTK_BOX (box));
+
+  if (center && !GLADE_IS_PLACEHOLDER (center))
+    gcenter = glade_widget_get_from_gobject (center);
+
+  glade_editable_block (GLADE_EDITABLE (box_editor));
+
+  if (use_center_child)
+    glade_command_push_group (_("Setting %s to use a center child"),
+                              glade_widget_get_name (gwidget));
+  else
+    glade_command_push_group (_("Setting %s to not use a center child"),
+                              glade_widget_get_name (gwidget));
+
+  /* If a project widget exists when were disabling center child, it needs
+   * to be removed first as a part of the issuing GladeCommand group
+   */
+  if (gcenter)
+    {
+      GList list;
+      list.prev = list.next = NULL;
+      list.data = gcenter;
+      glade_command_delete (&list);
+    }
+
+  property = glade_widget_get_property (gwidget, "use-center-child");
+  glade_command_set_property (property, use_center_child);
+
+  glade_command_pop_group ();
+
+  glade_editable_unblock (GLADE_EDITABLE (box_editor));
+
+  /* reload buttons and sensitivity and stuff... */
+  glade_editable_load (GLADE_EDITABLE (box_editor), gwidget);
 }
 
 GtkWidget *
