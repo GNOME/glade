5ab2189c804f8c329f79d7fda9d38899ca9eaf09
Matthias Clasen <mclasen@redhat.com>
2014-11-13 23:41:53 -0500

    Add support for GtkPopover
    This is pretty bare-bones at the moment.

---
+++ plugins/gtk+/glade-gtk-popover.c
@@ -0,0 +1,45 @@
+/*
+ * glade-gtk-popover.c - GladeWidgetAdaptor for GtkPopover
+ *
+ * Copyright (C) 2014 Red Hat, Inc
+ *
+ * Authors:
+ *      Matthias Clasen <mclasen@redhat.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include <config.h>
+#include <glib/gi18n-lib.h>
+#include <gladeui/glade.h>
+
+GObject *
+glade_gtk_popover_constructor (GType type,
+                               guint n_construct_properties,
+                               GObjectConstructParam * construct_properties)
+{
+  GladeWidgetAdaptor *adaptor;
+  GObject *ret_obj;
+
+  ret_obj = GWA_GET_OCLASS (GTK_TYPE_CONTAINER)->constructor
+      (type, n_construct_properties, construct_properties);
+
+  adaptor = GLADE_WIDGET_ADAPTOR (ret_obj);
+
+  glade_widget_adaptor_action_remove (adaptor, "add_parent");
+  glade_widget_adaptor_action_remove (adaptor, "remove_parent");
+
+  return ret_obj;
+}
