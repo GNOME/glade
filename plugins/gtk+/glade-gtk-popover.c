667f255a53820815117358e7e96641d49f4522dd
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2018-02-28 21:19:01 -0300

    Revert "GladeWidgetAdaptor: fix glade_widget_adaptor_get_parent_adaptor()"
    This reverts commit b9763bfada7c9ecc2532563a97c387f8c20962b5.
    
    Revert "GtkPopover: rework support code to fix bugs and warnings"
    
    This reverts commit d2bedbab24c64198b373345e973d00e1f9fdc33c.

--- plugins/gtk+/glade-gtk-popover.c
+++ plugins/gtk+/glade-gtk-popover.c
@@ -27,68 +27,6 @@
 
 #include "glade-popover-editor.h"
 
-#define GLADE_TYPE_GTK_POPOVER glade_gtk_popover_get_type ()
-G_DECLARE_FINAL_TYPE (GladeGtkPopover, glade_gtk_popover, GLADE, GTK_POPOVER, GtkPopover)
-
-struct _GladeGtkPopover
-{
-  GtkPopover parent_instance;
-};
-
-G_DEFINE_TYPE (GladeGtkPopover, glade_gtk_popover, GTK_TYPE_POPOVER)
-
-static void
-glade_gtk_popover_init (GladeGtkPopover *popover)
-{
-  gtk_popover_set_modal (GTK_POPOVER (popover), FALSE);
-  gtk_popover_set_relative_to (GTK_POPOVER (popover), NULL);
-}
-
-static void
-glade_gtk_popover_map (GtkWidget *widget)
-{
-  GtkWidgetClass *klass = g_type_class_peek_parent (glade_gtk_popover_parent_class);
-  klass->map (widget);
-}
-
-static void
-glade_gtk_popover_unmap (GtkWidget *widget)
-{
-  GtkWidgetClass *klass = g_type_class_peek_parent (glade_gtk_popover_parent_class);
-  klass->unmap (widget);
-}
-
-static gboolean
-glade_gtk_popover_button_event (GtkWidget *widget, GdkEventButton *event)
-{
-  return TRUE;
-}
-
-static gint
-glade_gtk_popover_key_press_event (GtkWidget *popover, GdkEventKey *event)
-{
-  if (event->keyval == GDK_KEY_Escape)
-    return TRUE;
-
-  return FALSE;
-}
-
-static void
-glade_gtk_popover_class_init (GladeGtkPopoverClass *klass)
-{
-  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
-
-  /* Ignore some events causing the popover to disappear from the workspace */
-  widget_class->key_press_event = glade_gtk_popover_key_press_event;
-  widget_class->button_press_event = glade_gtk_popover_button_event;
-  widget_class->button_release_event = glade_gtk_popover_button_event;
-
-  /* Make some warning go away */
-  widget_class->map = glade_gtk_popover_map;
-  widget_class->unmap = glade_gtk_popover_unmap;
-}
-
-
 GObject *
 glade_gtk_popover_constructor (GType type,
                                guint n_construct_properties,
@@ -118,3 +56,25 @@ glade_gtk_popover_create_editable (GladeWidgetAdaptor * adaptor,
     return GWA_GET_CLASS (GTK_TYPE_CONTAINER)->create_editable (adaptor, type);
 }
 
+static gint
+popover_key_press (GtkWidget   *popover,
+		   GdkEventKey *event,
+		   gpointer     user_data)
+{
+  if (event->keyval == GDK_KEY_Escape)
+    return TRUE;
+
+  return FALSE;
+}
+		   
+void
+glade_gtk_popover_post_create (GladeWidgetAdaptor * adaptor,
+			       GObject *popover, GladeCreateReason reason)
+{
+  /* Ignore some events causing the popover to disappear from the workspace
+   */
+  g_signal_connect (popover, "key-press-event",
+		    G_CALLBACK (popover_key_press), NULL);
+
+  GWA_GET_CLASS (GTK_TYPE_CONTAINER)->post_create (adaptor, popover, reason);
+}
