5b4cc54377224210e55da23c0c44bf27b5ff8e9e
Corentin Noël <corentin.noel@collabora.com>
2019-08-16 17:51:18 +0200

    gladeui: Use G_DECLARE_DERIVABLE_TYPE for GladeEditor

--- gladeui/glade-editor.h
+++ gladeui/glade-editor.h
@@ -6,41 +6,20 @@
 
 G_BEGIN_DECLS
 
-
-#define GLADE_TYPE_EDITOR            (glade_editor_get_type ())
-#define GLADE_EDITOR(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GLADE_TYPE_EDITOR, GladeEditor))
-#define GLADE_EDITOR_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GLADE_TYPE_EDITOR, GladeEditorClass))
-#define GLADE_IS_EDITOR(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GLADE_TYPE_EDITOR))
-#define GLADE_IS_EDITOR_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GLADE_TYPE_EDITOR))
-#define GLADE_EDITOR_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), GLADE_TYPE_EDITOR, GladeEditorClass))
-
-typedef struct _GladeEditor          GladeEditor;
-typedef struct _GladeEditorClass     GladeEditorClass;
-typedef struct _GladeEditorPrivate   GladeEditorPrivate;
+#define GLADE_TYPE_EDITOR glade_editor_get_type ()
+G_DECLARE_DERIVABLE_TYPE (GladeEditor, glade_editor, GLADE, EDITOR, GtkBox)
 
 /* The GladeEditor is a window that is used to display and modify widget
  * properties. The glade editor contains the details of the selected
  * widget for the selected project
  */
-struct _GladeEditor
-{
-  GtkBox vbox;  /* The editor is a vbox */
-
-  GladeEditorPrivate *priv;
-};
 
 struct _GladeEditorClass
 {
   GtkBoxClass parent_class;
 
-  void   (* glade_reserved1)   (void);
-  void   (* glade_reserved2)   (void);
-  void   (* glade_reserved3)   (void);
-  void   (* glade_reserved4)   (void);
+  gpointer padding[4];
 };
-
-
-GType        glade_editor_get_type           (void);
 
 GladeEditor *glade_editor_new                (void);
 void         glade_editor_load_widget        (GladeEditor       *editor,
