e15c7641aead60441a31fcbde61c9f24f6fd7751
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2020-10-06 20:34:11 -0300

    Fix windows build on msys
    Use -Wl,--export-all-symbols and -Wl,--no-undefined to export
    symbols on mingw
    
    GladeApp: use PACKAGE_NAME instead of PACKAGE
    
    Remove copyright symbol from resource files to avoid weird UTF error
    building with mingw on msys

--- src/glade-http.c
+++ src/glade-http.c
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2014, 2020 Juan Pablo Ugarte.
-   *
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.
-   *
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -22,6 +22,7 @@
 #include <glade-http.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 struct _GladeHTTPPrivate
 {
